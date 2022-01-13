bfbf26d49c149952b4caa4d00325793474432899
bingle-a81 <breath44@yandex.ru>
2022-01-13 22:57:33 +0300

    строки работа

--- py/lesson2/proba.py
+++ py/lesson2/proba.py
@@ -1,10 +1,16 @@
-Dict = {} 
-print("Empty Dictionary: ") 
-print(Dict) 
+# Dict = {} 
+# print("Empty Dictionary: ") 
+# print(Dict) 
   
-# Adding elements to dictionary one at a time 
-Dict[0] = 'Peter'
-Dict[2] = 'Joseph'
-Dict[3] = 'Ricky'
-print("\nDictionary after adding 3 elements: ") 
-print(Dict) 
+  
+# # Adding elements to dictionary one at a time 
+# Dict[0] = 'Peter'
+# Dict[2] = 'Joseph'
+# Dict[3] = 'Ricky'
+# print("\nDictionary after adding 3 elements: ") 
+# print(Dict) 
+
+t='dfjknzirodkkfjkall;lsd,,'
+print(t.find(';'))
+b=t.isalpha()
+print(type(b))
