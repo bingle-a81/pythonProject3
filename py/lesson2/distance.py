c9bcfea10e2ccac8c8f0fd9aa830b48ff7417287
bingle-a81 <breath44@yandex.ru>
2022-01-15 20:57:36 +0300

    distance

--- py/lesson2/distance.py
+++ py/lesson2/distance.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from pprint import pprint 
+
 # Есть словарь координат городов
 
 sites = {
@@ -18,6 +20,22 @@ moscow=sites['Moscow']
 london=sites['London']
 paris=sites['Paris']
 
+mos_lon=round(((moscow[0]-london[0])**2+(moscow[1]-london[1])**2)**0.5,4)
+mos_par=round(((moscow[0]-paris[0])**2+(moscow[1]-paris[1])**2)**0.5,4)
+lon_par=round(((london[0]-paris[0])**2+(london[1]-paris[1])**2)**0.5,4)
 
+distances['Moscow']={}
+distances['Moscow']['London']=mos_lon
+distances['Moscow']['Paris']=mos_par
 
-print(distances)
+distances['London']={}
+distances['London']['Moscow']=mos_lon
+distances['London']['Paris']=lon_par
+
+distances['Paris']={}
+distances['Paris']['Moscow']=mos_par
+distances['Paris']['London']=lon_par
+
+pprint(distances)
+
+print (distances['Moscow']['London'])
