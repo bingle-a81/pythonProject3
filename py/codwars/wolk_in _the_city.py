f96644b974b21038d0cefb43f9905fa43762c34c
bingle-a81 <breath44@yandex.ru>
2022-01-16 14:51:48 +0300

    walk

---
+++ py/codwars/wolk_in _the_city.py
@@ -0,0 +1,20 @@
+wlk=['w','e','w','e','w','e','w','e','w','e','w','e']
+
+def is_valid_walk(walk):
+    if len(walk)!=10:
+        print('should return False')
+    else:
+        north=walk.count('n')
+        south=walk.count('s')
+        east=walk.count('e')
+        west=walk.count('w')
+        if north==south and east==west:
+            print('should return True')
+        else:
+            print('should return False')
+    
+    #determine if walk is valid
+    # pass
+
+
+is_valid_walk(wlk)
