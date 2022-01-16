9a5ac774059f993b7f973a257ed273251d639096
bingle-a81 <breath44@yandex.ru>
2022-01-16 09:57:43 +0300

    zoo file

---
+++ py/lesson2/zoo.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# есть список животных в зоопарке
+
+zoo = ['lion', 'kangaroo', 'elephant', 'monkey', ]
+
+# посадите медведя (bear) между львом и кенгуру
+#  и выведите список на консоль
+zoo.insert(1,'bear')
+
+# добавьте птиц из списка birds в последние клетки зоопарка
+birds = ['rooster', 'ostrich', 'lark', ]
+#  и выведите список на консоль
+zoo=zoo+birds
+
+# уберите слона
+#  и выведите список на консоль
+# TODO здесь ваш код
+zoo.pop(3)
+
+# выведите на консоль в какой клетке сидит лев (lion) и жаворонок (lark).
+# Номера при выводе должны быть понятны простому человеку, не программисту.
+# TODO здесь ваш код
+def kletka(stro):
+    if stro in zoo:
+        print (f'клетка {stro} = ',str(zoo.index(stro)+1))
+    else:
+        print(f'{stro} такого нет')
+
+kletka('lion')
+kletka('lak')
+
