b94fe14feb6de274487983e2b11d6761a334d125
bingle-a81 <breath44@yandex.ru>
2022-01-20 05:28:56 +0300

    20.01.22

--- py/codwars/Summa_dvukh_ naimenshikh_chisel.py
+++ py/codwars/Summa_dvukh_ naimenshikh_chisel.py
@@ -4,14 +4,14 @@ massiv = [-10, 343445353.6, 3453445, 3453545353453]
 def sum_two_smallest_numbers(numbers):
     k = a1 = a2 = 0
     numbers.sort()
-    for num in range(len(numbers)):
-        if numbers[num] < 0 or True != isinstance(numbers[num], int):
+    for num in numbers:
+        if num < 0 or True != isinstance(num, int):
             continue
         elif k == 0:
-            a1 = numbers[num]
+            a1 = num
             k += 1
         else:
-            a2 = numbers[num]
+            a2 = num
             break
     return a1 + a2
 
--- py/codwars/найти не четное.py
+++ py/codwars/найти не четное.py
@@ -3,11 +3,11 @@ a=[11,160, 3, 1719, 19, 11, 13]
 def find_outlier(integers):
     k0=[]
     k1=[]
-    for i in range(len(integers)):
-        if int(integers[i]) % 2 == 0:
-            k0.append(integers[i])
+    for i in integers:
+        if int(i) % 2 == 0:
+            k0.append(i)
         else:
-            k1.append(integers[i])
+            k1.append(i)
 
     if len(k0)==1:
         return k0[0]
